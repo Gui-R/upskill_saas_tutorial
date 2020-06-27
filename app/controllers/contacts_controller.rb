471f0f78151c3ba2f99eb42cd3ae9c7dc8a2f3bf
Guilherme Rodrigues <guilhermemrodri@gmail.com>
2020-06-27 22:34:27 +0000

    Added create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Mensagem enviada!"
+    else
+      redirect_to new_contact_path, notice: "Erro no sistema alguém me desconfigurou... (tente novamente)"
+    end
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :mensagem)
+    end
 end
