ed4557a707466020a956c6a49875e598b07758df
Guilherme Rodrigues <guilhermemrodri@gmail.com>
2020-07-04 00:12:31 +0000

    Improve error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      redirect_to new_contact_path, notice: "Mensagem enviada!"
+      flash[:sucess] = "Mensagem enviada"
+      redirect_to new_contact_path
     else
-      redirect_to new_contact_path, notice: "Erro no sistema alguém me desconfigurou... (tente novamente)"
+      flash[:error] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path
     end
   end
   
